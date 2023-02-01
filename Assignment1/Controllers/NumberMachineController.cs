f15ab8c6bbe021054c003c6c375d87d17e755020
wyhzest@gmail.com <wisestMe>
2023-01-31 22:45:07 -0500

    Add project files.

---
+++ Assignment1/Controllers/NumberMachineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1.Controllers
+{
+    public class NumberMachineController : ApiController
+    {
+        public int Get(int id)
+        {
+            return id + 7 * 2 / 4 - 10;
+        }
+    }
+}
